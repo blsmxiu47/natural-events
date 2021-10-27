c499205cf17e58dc8c5324af5be26aabcb8d5a0c
Wes Warriner <fridgehistory@gmail.com>
2021-10-27 10:58:05 -0700

    create vue app

---
+++ src/components/Map.vue
@@ -0,0 +1,30 @@
+<template>
+  <l-map style="height: 500px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+// import L from 'leaflet';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+
+export default {
+  name: 'Map',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 3,
+      center: [0, 0],
+      markerLatLng: [0, 0]
+    };
+  }
+}
+</script>
