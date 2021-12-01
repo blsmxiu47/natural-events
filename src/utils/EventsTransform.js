3ff8376a682003b1c0db01e16c69ceb58386c265
Wes Warriner <fridgehistory@gmail.com>
2021-12-01 11:08:59 -0800

    lint, fix utils js files

--- src/utils/EventsTransform.js
+++ src/utils/EventsTransform.js
@@ -1,11 +1,10 @@
 const eventsTransform = (events) => {
-    console.log("eventsTransform...", events);
-    events.map(ev => {
-        ev.geometry.map(geo => {
-            geo.coordinates.reverse()
-        })
+  events.map(ev => {
+    return ev.geometry.map(geo => {
+      return geo.coordinates.reverse()
     })
-    return events
+  })
+  return events
 }
 
 export default eventsTransform
